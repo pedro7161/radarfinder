c9093ec7574aaec776af9a2fb3a77616dd1f5d9b
Pedro Pinto <pedro.ramos9@hotmail.com>
2025-07-16 17:44:26 +0100

    Added default primeng menu

--- src/app/shared/menu/navbar/navbar.component.ts
+++ src/app/shared/menu/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 
+import { MenubarModule } from 'primeng/menubar';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  items = [
+    {
+      label: 'File',
+      items: [
+        { label: 'New', icon: 'pi pi-fw pi-plus' },
+        { label: 'Open', icon: 'pi pi-fw pi-download' }
+      ]
+    },
+    {
+      label: 'Edit',
+      items: [
+        { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+        { label: 'Redo', icon: 'pi pi-fw pi-repeat' }
+      ]
+    }
+  ];
 }
