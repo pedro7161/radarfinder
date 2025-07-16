36d44f43120f0c456d93e3a17720b08034bd7bd7
Pedro Pinto <pedro.ramos9@hotmail.com>
2025-07-16 16:58:30 +0100

    WIP: before Angular 20 upgrade

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })]
 };
