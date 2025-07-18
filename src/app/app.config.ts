fd0d6bc4575f847ec1267ed7c4481fcdf53e5017
Pedro Pinto <pedro.ramos9@hotmail.com>
2025-07-18 15:25:59 +0100

    WIP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,12 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
         providePrimeNG({
             theme: {
